9596284d784efe6a620f911858d4aded4e614632
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-08-20 13:32:41 +0500

    𝑷-𝑴𝑫

--- plugins/tools-obfuscator.js
+++ plugins/tools-obfuscator.js
@@ -1,12 +1,12 @@
 import JavaScriptObfuscator from 'javascript-obfuscator'
 
 let handler = async(m, { conn, text }) => {
-if (!text) return m.reply(`*${lenguajeGB['smsAvisoMG']()}Past the code you want to encrypt*`) 
+if (!text) return m.reply(`🚩*${mssg.example} Past the code you want to encrypt*`) 
 function obfuscateCode(code) {
   return JavaScriptObfuscator.obfuscate(code, { compact: false, controlFlowFlattening: true, deadCodeInjection: true, simplify: true, numbersToExpressions: true }).getObfuscatedCode();
 }
 let obfuscatedCode = await obfuscateCode(text);
 conn.sendMessage(m.chat, {text: obfuscatedCode}, {quoted: m});
 }
-handler.command = /^(ofuscar|ofuscador|obs)$/i
+handler.command = /^(ofuscar|obfuscator|obs)$/i
 export default handler
