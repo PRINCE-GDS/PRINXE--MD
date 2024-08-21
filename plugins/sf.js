8b3c39e21b240750ef0f428bd6bd7381567ced62
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-08-21 09:43:32 +0500

    𝑷-𝑴𝑫

---
+++ plugins/sf.js
@@ -0,0 +1,14 @@
+import fs from 'fs' 
+ let handler = async (m, { text, usedPrefix, command }) => { 
+     if (!text) throw `uhm.. where is the text?\n\nusage:\n${usedPrefix + command} <text>\n\nexample:\n${usedPrefix + command} plugins/sweeto.js` 
+     if (!m.quoted.text) throw `reply to the message!` 
+     let path = `${text}` 
+     await fs.writeFileSync(path, m.quoted.text) 
+     m.reply(`stored in ${path}`) 
+ } 
+ handler.help = ['sf'].map(v => v + ' <text>') 
+ handler.tags = ['owner'] 
+ handler.command = /^sf$/i 
+  
+ handler.rowner = true 
+ export default handler
