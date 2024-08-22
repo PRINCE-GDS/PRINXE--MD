576fda6379daf633e609421aa45ee2d5172de196
MANNO-GDS <153306254+MANNO-GDS@users.noreply.github.com>
2024-08-22 13:02:39 +0500

    💌 𝙏𝙃𝙀-𝙈𝘼𝙉𝙉𝙊-𝙈𝘿 💌

--- plugins/bebot-botlist.js
+++ plugins/bebot-botlist.js
@@ -4,7 +4,7 @@ async function handler(m, { usedPrefix }) {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map(conn => conn.user)])]
   let b = users.map((v, i) => `_*${i + 1}.*_ *${v.name}*\nwa.me/${v.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}help`).join('\n')
  m.reply(` 
-≡ *List of Active Sub Bots*
+≡ *𝙇𝙄𝙎𝙏 𝙊𝙁 𝘼𝘾𝙏𝙄𝙑𝙀 𝙎𝙐𝘽 𝘽𝙊𝙏𝙎*
 
 ▢ *Total:* ${users.length} 
 
