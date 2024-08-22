45a379fd90afd9573f3480db6f2f812d5ba73fb1
MANNO-GDS <153306254+MANNO-GDS@users.noreply.github.com>
2024-08-22 14:27:19 +0500

    💌 𝙏𝙃𝙀-𝙈𝘼𝙉𝙉𝙊-𝙈𝘿 💌

--- plugins/gp-kick.js
+++ plugins/gp-kick.js
@@ -7,7 +7,7 @@ if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`✅ 𝐔𝐬𝐞𝐫 𝐡𝐚𝐬 𝐛𝐞𝐞𝐧 𝐊𝐢𝐜𝐤𝐞𝐝👋🏻`) 
+m.reply(`𝙉𝙄𝙆𝙇 𝘽𝙊𝙎𝙍𝙀 𝙆 🤣🥵💭`) 
 
 }
 
