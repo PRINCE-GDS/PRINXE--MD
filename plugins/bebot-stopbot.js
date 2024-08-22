f9509f038743ed7d267b3917ecf2d53cb8de5fac
MANNO-GDS <153306254+MANNO-GDS@users.noreply.github.com>
2024-08-22 13:08:12 +0500

    💌 𝙏𝙃𝙀-𝙈𝘼𝙉𝙉𝙊-𝙈𝘿 💌

--- plugins/bebot-stopbot.js
+++ plugins/bebot-stopbot.js
@@ -1,10 +1,10 @@
 
 let handler = async (m, { conn }) => {
   if (global.conn.user.jid === conn.user.jid) {
-   await conn.reply(m.chat, '✳️ Why not go directly to the terminal?', m);
+   await conn.reply(m.chat, '💭 𝙒𝙃𝙔 𝙉𝙊𝙏 𝙂𝙊 𝘿𝙄𝙍𝙄𝘾𝙏𝙇𝙔 𝙏𝙊 𝙏𝙃𝙀 𝙏𝙀𝙍𝙈𝙄𝙉𝘼𝙇 ?', m);
   } else {
     //Si el número no coincide, se detiene el bot.
-    await conn.reply(m.chat, `✅ Bot disconnected`, m);
+    await conn.reply(m.chat, `✅ 𝘽𝙊𝙏 𝙄𝙎 𝘾𝙊𝙉𝙉𝙀𝘾𝙏𝙀𝘿`, m);
     conn.ws.close();
   }
 };
