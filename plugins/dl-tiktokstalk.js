cdb0c5a87b4480913835067d42110e570d54e9ee
MANNO-GDS <153306254+MANNO-GDS@users.noreply.github.com>
2024-08-22 14:12:23 +0500

    💌 𝙏𝙃𝙀-𝙈𝘼𝙉𝙉𝙊-𝙈𝘿 💌

--- plugins/dl-tiktokstalk.js
+++ plugins/dl-tiktokstalk.js
@@ -1,18 +1,16 @@
-
 import fg from 'api-dylux'
 let handler = async (m, { conn, text, args }) => {
 	
-  if (!text) throw `✳️Enter the Username of a TikTok user `
+  if (!text) throw `💭 𝙀𝙉𝙏𝙀𝙍 𝙏𝙃𝙀 𝙐𝙎𝙀𝙍𝙉𝘼𝙈𝙀 𝙊𝙁 𝙏𝙄𝙆𝙏𝙊𝙆 𝙐𝙎𝙀𝙍`
   let res = await fg.ttStalk(args[0])
   let txt = `
-┌──「 *TIKTOK STALK* 
-▢ *🔖Number:* ${res.name}
-▢ *🔖Username:* ${res.username}
-▢ *👥followers:* ${res.followers}
-▢ *🫂following:* ${res.following}
-▢ *📌Desc:* ${res.desc}
-
-▢ *🔗 Link* : https://tiktok.com/${res.username}
+┌──「 *𝙏𝙄𝙆𝙏𝙊𝙆-𝙎𝙏𝘼𝙇𝙆* 
+▢ *🔖 𝙉𝙐𝙈𝘽𝙀𝙍:* ${res.name}
+▢ *💌 𝙐𝙎𝙀𝙍𝙉𝘼𝙈𝙀:* ${res.username}
+▢ *👥 𝙁𝙊𝙇𝙇𝙊𝙒𝙀𝙍𝙎:* ${res.followers}
+▢ *🫂 𝙁𝙊𝙇𝙇𝙒𝙄𝙉𝙂𝙎:* ${res.following}
+▢ *📌 𝘿𝙀𝙎𝘾:* ${res.desc}
+▢ *🔗 𝙇𝙄𝙉𝙆* : https://tiktok.com/${res.username}
 └────────────`
   await conn.sendFile(m.chat, res.profile, 'tt.png', txt, m)
 }
