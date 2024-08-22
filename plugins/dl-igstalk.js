f2058ce7afc20b6ac90b95a360af3b0db208ed66
MANNO-GDS <153306254+MANNO-GDS@users.noreply.github.com>
2024-08-22 13:49:06 +0500

    💌 𝙏𝙃𝙀-𝙈𝘼𝙉𝙉𝙊-𝙈𝘿 💌

--- plugins/dl-igstalk.js
+++ plugins/dl-igstalk.js
@@ -2,16 +2,16 @@
 import fg from 'api-dylux'
 let handler= async (m, { conn, args, text, usedPrefix, command }) => {
 	
-    if (!args[0]) throw `✳️ Enter the Instagram Username\n\n📌Example: ${usedPrefix + command} princegds` 
+    if (!args[0]) throw `💭 𝙀𝙉𝙏𝙀𝙍 𝙏𝙃𝙀 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈 𝙇𝙄𝙉𝙆 𝙐𝙎𝙀𝙍𝙉𝘼𝙈𝙀\n\n📌Example: ${usedPrefix + command} 𝙈𝘼𝙉𝙉𝙊-𝙂𝘿𝙎` 
     let res = await fg.igStalk(args[0])
     let te = `
-┌──「 *STALKING* 
-▢ *🔖Number:* ${res.name} 
-▢ *🔖Username:* ${res.username}
-▢ *👥followers:* ${res.followersH}
-▢ *🫂following:* ${res.followingH}
-▢ *📌Bio:* ${res.description}
-▢ *🏝️Posts:* ${res.postsH}
+┌──「 *𝙎𝙏𝘼𝙇𝙆𝙄𝙉𝙂* 
+▢ *🔖𝙉𝙐𝙈𝘽𝙀𝙍:* ${res.name} 
+▢ *🔖𝙐𝙎𝙀𝙍𝙉𝘼𝙈𝙀:* ${res.username}
+▢ *👥𝙁𝙊𝙇𝙇𝙊𝙒𝙀𝙍𝙎:* ${res.followersH}
+▢ *🫂𝙁𝙊𝙇𝙇𝙒𝙄𝙉𝙂:* ${res.followingH}
+▢ *📌𝘽𝙄𝙊:* ${res.description}
+▢ *🏝️𝙋𝙊𝙎𝙏𝙎:* ${res.postsH}
 └────────────`
 
      await conn.sendFile(m.chat, res.profilePic, 'tt.png', te, m)
