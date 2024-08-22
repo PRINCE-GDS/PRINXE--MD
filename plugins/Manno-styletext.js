b50e6a43c997ce7cb972bd32b88e74d82b09f23a
MANNO-GDS <153306254+MANNO-GDS@users.noreply.github.com>
2024-08-22 23:04:54 +0500

    💌 𝙏𝙃𝙀-𝙈𝘼𝙉𝙉𝙊-𝙈𝘿 💌

--- plugins/Manno-styletext.js
+++ plugins/Manno-styletext.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, text }) => {
 
   // If no key and text provided, show all styles of a default text
   if (words.length === 0 || !key || !textToStyle) {
-    let defaultText = 'MANNO BOT'
+    let defaultText = 'GURU BOT'
     let styledTexts = await Promise.all(
       [...Array(34).keys()].map(i => stylizeText(defaultText, i + 1))
     )
