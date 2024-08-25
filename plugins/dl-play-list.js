e0592bd450de560b808e75324e397cdbd1b69813
MANNO-GDS <153306254+MANNO-GDS@users.noreply.github.com>
2024-08-25 08:27:54 +0500

    💌 𝙏𝙃𝙀-𝙈𝘼𝙉𝙉𝙊-𝙈𝘿 💌

--- plugins/dl-play-list.js
+++ plugins/dl-play-list.js
@@ -1,155 +1,119 @@
-import fetch from 'node-fetch'
-import ytdl from 'youtubedl-core'
-import yts from 'youtube-yts'
-import fs from 'fs'
-import { pipeline } from 'stream'
-import { promisify } from 'util'
-import os from 'os'
-
-const streamPipeline = promisify(pipeline)
-
-const handler = async (m, { conn, command, text, args, usedPrefix }) => {
-  if (!text) throw `give a text to search Example: *${usedPrefix + command}* sefali odia song`
-  conn.GURUPLAY = conn.GURUPLAY ? conn.GURUPLAY : {}
-  await conn.reply(m.chat, wait, m)
-  const result = await searchAndDownloadMusic(text)
-  const infoText = `🎶💌 *•┈┈••✦❀𝙈𝘼𝙉𝙉𝙊-𝙋𝙇𝙄𝙔𝙀𝙍❀✦••┈┈•* 💌🎶\n\n\n [ 🌍 𝙍𝙀𝙋𝙇𝙔 𝙉𝙐𝙈𝘽𝙀𝙍 𝙏𝙊 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 ]. \n\n`
-
-  const orderedLinks = result.allLinks.map((link, index) => {
-    const sectionNumber = index + 1
-    const { title, url } = link
-    return `*${sectionNumber}.* ${title}`
-  })
-
-  const orderedLinksText = orderedLinks.join('\n\n')
-  const fullText = `${infoText}\n\n${orderedLinksText}`
-  const { key } = await conn.reply(m.chat, fullText, m)
-  conn.GURUPLAY[m.sender] = {
-    result,
-    key,
-    timeout: setTimeout(() => {
-      conn.sendMessage(m.chat, {
-        delete: key,
-      })
-      delete conn.GURUPLAY[m.sender]
-    }, 150 * 1000),
-  }
-}
-
-handler.before = async (m, { conn }) => {
-  conn.GURUPLAY = conn.GURUPLAY ? conn.GURUPLAY : {}
-  if (m.isBaileys || !(m.sender in conn.GURUPLAY)) return
-  const { result, key, timeout } = conn.GURUPLAY[m.sender]
-
-  if (!m.quoted || m.quoted.id !== key.id || !m.text) return
-  const choice = m.text.trim()
-  const inputNumber = Number(choice)
-  if (inputNumber >= 1 && inputNumber <= result.allLinks.length) {
-    const selectedUrl = result.allLinks[inputNumber - 1].url
-    console.log('selectedUrl', selectedUrl)
-    let title = generateRandomName()
-    const audioStream = ytdl(selectedUrl, {
-      filter: 'audioonly',
-      quality: 'highestaudio',
-    })
-
-    const tmpDir = os.tmpdir()
-
-    const writableStream = fs.createWriteStream(`${tmpDir}/${title}.mp3`)
-
-    await streamPipeline(audioStream, writableStream)
-
-    const doc = {
-      audio: {
-        url: `${tmpDir}/${title}.mp3`,
-      },
-      mimetype: 'audio/mpeg',
-      ptt: false,
-      waveform: [100, 0, 0, 0, 0, 0, 100],
-      fileName: `${title}`,
+const _0x2c134e = _0x5482;
+(function (_0x46cb10, _0x3683f3) {
+    const _0x61a562 = _0x5482, _0x391b04 = _0x46cb10();
+    while (!![]) {
+        try {
+            const _0x3d3c68 = parseInt(_0x61a562(0x142)) / (0x12 * 0x8f + -0x2b1 + 0x13a * -0x6) + parseInt(_0x61a562(0x12f)) / (-0x37 * -0x98 + -0x89 * -0x1 + 0x212f * -0x1) * (-parseInt(_0x61a562(0x138)) / (0x1af7 + -0x109a + 0x19 * -0x6a)) + -parseInt(_0x61a562(0x11c)) / (-0x7 * -0x1fd + -0x7f * -0x1 + -0xe66) * (-parseInt(_0x61a562(0x127)) / (-0xa78 + -0x17 * 0x89 + 0x16cc * 0x1)) + parseInt(_0x61a562(0x130)) / (0x371 + -0x2412 + 0x20a7) * (-parseInt(_0x61a562(0x137)) / (0xa75 + -0x25f5 + 0x3 * 0x92d)) + parseInt(_0x61a562(0x12d)) / (-0xf * 0x95 + 0x2e9 * -0x9 + 0x22f4) * (parseInt(_0x61a562(0x140)) / (-0x1f41 + 0x122f + 0xd1b)) + -parseInt(_0x61a562(0x12e)) / (-0xb * -0x35d + 0x87 * -0x16 + -0x195b) + parseInt(_0x61a562(0x120)) / (-0xf4e + 0xc79 + 0x2e0) * (-parseInt(_0x61a562(0x126)) / (0x1e * 0xf4 + 0x978 + -0x2604 * 0x1));
+            if (_0x3d3c68 === _0x3683f3)
+                break;
+            else
+                _0x391b04['push'](_0x391b04['shift']());
+        } catch (_0x5cf1b4) {
+            _0x391b04['push'](_0x391b04['shift']());
+        }
     }
-
-    await conn.sendMessage(m.chat, doc, { quoted: m })
-  } else {
-    m.reply(
-      'Invalid sequence number. Please select the appropriate number from the list above.\nBetween 1 to ' +
-        result.allLinks.length
-    )
-  }
+}(_0x1959, -0x13e6 * 0x17 + 0x23 * -0xa421 + 0x25e00a));
+import _0x25c5fc from 'yt-search';
+function _0x5482(_0x53d7ea, _0x42d2b1) {
+    const _0x4e91d1 = _0x1959();
+    return _0x5482 = function (_0x580182, _0x70d0e) {
+        _0x580182 = _0x580182 - (-0x1019 + 0x4c4 * 0x7 + -0x1028);
+        let _0x12cca2 = _0x4e91d1[_0x580182];
+        return _0x12cca2;
+    }, _0x5482(_0x53d7ea, _0x42d2b1);
 }
-
-handler.help = ['playlist']
-handler.tags = ['downloader']
-handler.command = /^(playlist)$/i
-handler.limit = false
-export default handler
-
-function formatBytes(bytes, decimals = 2) {
-  if (bytes === 0) return '0 B'
-  const k = 1024
-  const dm = decimals < 0 ? 0 : decimals
-  const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-  const i = Math.floor(Math.log(bytes) / Math.log(k))
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
-}
-
-async function searchAndDownloadMusic(query) {
-  try {
-    const { videos } = await yts(query)
-    if (!videos.length) return 'Sorry, no video results were found for this search.'
-
-    const allLinks = videos.map(video => ({
-      title: video.title,
-      url: video.url,
-    }))
-
-    const jsonData = {
-      title: videos[0].title,
-      description: videos[0].description,
-      duration: videos[0].duration,
-      author: videos[0].author.name,
-      allLinks: allLinks,
-      videoUrl: videos[0].url,
-      thumbnail: videos[0].thumbnail,
+let handler = async (_0x2c3f8f, {
+    conn: _0x2f9a38,
+    usedPrefix: _0x1a810d,
+    text: _0x201b2b,
+    args: _0x592aee,
+    command: _0x5ba316
+}) => {
+    const _0x2ca55b = _0x5482, _0xcb6393 = {
+            'LTBtO': _0x2ca55b(0x133),
+            'dfWAd': _0x2ca55b(0x13b)
+        };
+    if (!_0x201b2b)
+        throw _0x2ca55b(0x141) + mssg[_0x2ca55b(0x11f)] + '\x20*' + (_0x1a810d + _0x5ba316) + _0x2ca55b(0x123);
+    _0x2c3f8f[_0x2ca55b(0x145)]('⏳');
+    let _0x7dc3a = await _0x25c5fc(_0x201b2b), _0x16c555 = _0x7dc3a[_0x2ca55b(0x11d)], _0x25302c = [];
+    for (let _0x2ddf2f in _0x16c555) {
+        let _0x335ec0 = _0x16c555[_0x2ddf2f];
+        _0x25302c[_0x2ca55b(0x143)]({
+            'title': _0x2ddf2f + '┃\x20' + _0x335ec0[_0x2ca55b(0x136)],
+            'rows': [
+                {
+                    'header': _0xcb6393['LTBtO'],
+                    'title': '',
+                    'description': _0x2ca55b(0x135) + mssg[_0x2ca55b(0x11e)] + ':\x20' + _0x335ec0['timestamp'] + _0x2ca55b(0x128) + mssg['views'] + ':\x20' + _0x335ec0[_0x2ca55b(0x12a)] + _0x2ca55b(0x139) + mssg[_0x2ca55b(0x136)] + _0x2ca55b(0x12c) + _0x335ec0['title'] + _0x2ca55b(0x12b) + mssg[_0x2ca55b(0x11b)] + ':\x20' + _0x335ec0['ago'] + '\x0a',
+                    'id': _0x1a810d + _0x2ca55b(0x13f) + _0x335ec0[_0x2ca55b(0x125)]
+                },
+                {
+                    'header': _0xcb6393['dfWAd'],
+                    'title': '',
+                    'description': _0x2ca55b(0x135) + mssg[_0x2ca55b(0x11e)] + ':\x20' + _0x335ec0[_0x2ca55b(0x13d)] + _0x2ca55b(0x128) + mssg[_0x2ca55b(0x12a)] + ':\x20' + _0x335ec0[_0x2ca55b(0x12a)] + _0x2ca55b(0x139) + mssg['title'] + _0x2ca55b(0x12c) + _0x335ec0[_0x2ca55b(0x136)] + _0x2ca55b(0x12b) + mssg[_0x2ca55b(0x11b)] + ':\x20' + _0x335ec0[_0x2ca55b(0x121)] + '\x0a',
+                    'id': _0x1a810d + _0x2ca55b(0x134) + _0x335ec0[_0x2ca55b(0x125)]
+                }
+            ]
+        });
     }
-
-    return jsonData
-  } catch (error) {
-    return 'Error: ' + error.message
-  }
+    await _0x2f9a38[_0x2ca55b(0x13c)](_0x2c3f8f[_0x2ca55b(0x124)], '\x20\x20≡\x20𝙈𝘼𝙉𝙉𝙊\x20𝗣𝗟𝗔𝗬𝗟𝗜𝗦𝗧\x20💌', '\x0a\x20📁\x20\x20𝗥𝗘𝗦𝗨𝗟𝗧𝗦:\x0a\x20*' + _0x201b2b + '*', '𝗖𝗹𝗶𝗰𝗸\x20𝗵𝗲𝗿𝗲♥️\x20', _0x16c555[0x2130 + -0x1a * 0xc0 + -0x490 * 0x3][_0x2ca55b(0x13e)], _0x25302c, _0x2c3f8f);
+};
+handler[_0x2c134e(0x13a)] = [_0x2c134e(0x129)], handler['tags'] = [_0x2c134e(0x144)], handler[_0x2c134e(0x122)] = [
+    _0x2c134e(0x131),
+    _0x2c134e(0x129),
+    _0x2c134e(0x146)
+], handler[_0x2c134e(0x132)] = ![];
+function _0x1959() {
+    const _0x1cc0de = [
+        '🎥\x20𝗠𝗣4',
+        'sendList',
+        'timestamp',
+        'image',
+        'yta\x20',
+        '2841714jumHAk',
+        '✳️\x20',
+        '392123DDKgCE',
+        'push',
+        'downloader',
+        'react',
+        'playlista',
+        'aploud',
+        '12552lYnFpD',
+        'videos',
+        'duration',
+        'example',
+        '1931127jWfsQS',
+        'ago',
+        'command',
+        '*\x20bardo\x20jholi\x20Meri\x20Tahir\x20Qadri\x20Naat*',
+        'chat',
+        'url',
+        '48mtebsQ',
+        '2090sJdTHz',
+        '\x0a𓅓\x20👀\x20',
+        'playlist',
+        'views',
+        '\x0a𓅓\x20📆\x20',
+        '\x20:\x20',
+        '8uwkxKJ',
+        '582610MgnaZc',
+        '182VIVfEK',
+        '534VewDJV',
+        'playvid2',
+        'disabled',
+        '🎶\x20𝗠𝗣3',
+        'ytv\x20',
+        '𓅓\x20⌚\x20',
+        'title',
+        '4466RYWcbG',
+        '10149AroBfM',
+        '\x0a𓅓\x20📌\x20',
+        'help'
+    ];
+    _0x1959 = function () {
+        return _0x1cc0de;
+    };
+    return _0x1959();
 }
-
-async function fetchVideoBuffer() {
-  try {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-    })
-    return await response.buffer()
-  } catch (error) {
-    return null
-  }
-}
-
-function generateRandomName() {
-  const adjectives = [
-    'happy',
-    'sad',
-    'funny',
-    'brave',
-    'clever',
-    'kind',
-    'silly',
-    'wise',
-    'gentle',
-    'bold',
-  ]
-  const nouns = ['cat', 'dog', 'bird', 'tree', 'river', 'mountain', 'sun', 'moon', 'star', 'cloud']
-
-  const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)]
-  const randomNoun = nouns[Math.floor(Math.random() * nouns.length)]
-
-  return randomAdjective + '-' + randomNoun
-}
+export default handler;
