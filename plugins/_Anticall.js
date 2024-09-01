b9604964c2284ca1c91928255d61c2b56fc20ab7
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-09-01 07:30:05 +0500

    𝑷-𝑴𝑫

--- plugins/_Anticall.js
+++ plugins/_Anticall.js
@@ -1,19 +1,13 @@
-const { WAMessageStubType } = (await import('@WhiskeySockets/Baileys')).default
-const isNumber = x => typeof x === 'number' && !isNaN(x)
-const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
-    clearTimeout(this)
-    resolve()
-}, ms))
+conn.ev.on("call", async (json) => {
+    for (let id of json) {
+        if (id.status === "offer") {
+            let msg = await conn.sendMessage(id.from, { text: "`Anticall Feature is active so That's why you have been blocked for calling the bot contact the to unblock yourself from bot number `\n\nhttps://whatsapp.com/channel/0029VaKNbWkKbYMLb61S1v11" });
 
-export async function all(m) {
-	if (m.fromMe && m.isBaileys) return !0
-	let text;
-	let setting = global.db.data.settings[this.user.jid]
-	if(!setting.anticall) return 
-	
-	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO )) {
-	    await delay(1000) 
-		await conn.reply(m.chat, `You Are Blocked For Calling Bots!!`, m)
-		return conn.updateBlockStatus(m.chat, "block") 
-	}
-}
+            conn.sendContact(id.from, global.owner, msg);
+            await conn.rejectCall(id.id, id.from);
+
+            // Block the user
+            await conn.updateBlockStatus(id.from, 'block');
+        }
+    }
+});
