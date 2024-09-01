914afef038d9f94e7b4fb7dad9d564cb11930178
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-09-01 08:11:28 +0500

    𝑷-𝑴𝑫

--- plugins/_Anticall.js
+++ plugins/_Anticall.js
@@ -1,7 +1,7 @@
 conn.ev.on("call", async (json) => {
     for (let id of json) {
         if (id.status === "offer") {
-            let msg = await conn.sendMessage(id.from, { text: "`Anticall Feature is active so That's why you have been blocked for calling the bot contact the to unblock yourself from bot number `\n\nhttps://whatsapp.com/channel/0029VaKNbWkKbYMLb61S1v11" });
+            let msg = await conn.sendMessage(id.from, { text: "`Anticall Feature is active`\nThat's why you have been blocked for calling the bot`\n`Contact the BOT OWNER to unblock yourself from bot number`\n\nhttps://whatsapp.com/channel/0029VaKNbWkKbYMLb61S1v11" });
 
             conn.sendContact(id.from, global.owner, msg);
             await conn.rejectCall(id.id, id.from);
