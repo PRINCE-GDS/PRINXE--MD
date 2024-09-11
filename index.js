cb1d5d62872d437eea2cafa64301969490e5af0c
MANNO-GDS <153306254+MANNO-GDS@users.noreply.github.com>
2024-09-11 09:41:28 +0500

    💌 𝙏𝙃𝙀-𝙈𝘼𝙉𝙉𝙊-𝙈𝘿 💌

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
-console.log('✅ Prince bot Starting...')
-
-import { join, dirname } from 'path'
-import { createRequire } from 'module';
-import { fileURLToPath } from 'url'
-import { setupMaster, fork } from 'cluster'
-import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts';
-import { createInterface } from 'readline'
-import yargs from 'yargs'
-import express from 'express'
 import chalk from 'chalk'
+import { spawn } from 'child_process'
+import express from 'express'
+import figlet from 'figlet'
+import fs from 'fs'
 import path from 'path'
-import os from 'os'
-import { promises as fsPromises } from 'fs'
+import { fileURLToPath } from 'url';
 
-// https://stackoverflow.com/a/50052194
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
-const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
-const { say } = cfonts
-const rl = createInterface(process.stdin, process.stdout)
+figlet(
+  'MANNO BOT',
+  {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+  },
+  (err, data) => {
+    if (err) {
+      console.error(chalk.red('Figlet error:', err))
+      return
+    }
+    console.log(chalk.yellow(data))
+  }
+)
+
+figlet(
+  'Advanced Whatsapp User Bot',
+  {
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+  },
+  (err, data) => {
+    if (err) {
+      console.error(chalk.red('Figlet error:', err))
+      return
+    }
+    console.log(chalk.magenta(data))
+  }
+)
 
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 5000
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'Assets')));
 
@@ -31,31 +50,26 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(chalk.green(`🌐Port ${port} is open`))
+  console.log(chalk.green(`Port ${port} is open`))
 })
 
-var isRunning = false
+let isRunning = false
 
 async function start(file) {
   if (isRunning) return
   isRunning = true
+
   const currentFilePath = new URL(import.meta.url).pathname
-  let args = [join(__dirname, file), ...process.argv.slice(2)]
-  say([process.argv[0], ...args].join(' '), {
-    font: 'console',
-    align: 'center',
-    gradient: ['red', 'magenta']
+  const args = [path.join(path.dirname(currentFilePath), file), ...process.argv.slice(2)]
+  const p = spawn(process.argv[0], args, {
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
   })
-  setupMaster({
-    exec: args[0],
-    args: args.slice(1),
-  })
-  let p = fork()
+
   p.on('message', data => {
-    console.log('[RECEIVED]', data)
+    console.log(chalk.cyan(`✔️RECEIVED ${data}`))
     switch (data) {
       case 'reset':
-        p.process.kill()
+        p.kill()
         isRunning = false
         start.apply(this, arguments)
         break
@@ -64,57 +78,54 @@ async function start(file) {
         break
     }
   })
-  //---
-  p.on('exit', (_, code) => {
+
+  p.on('exit', code => {
     isRunning = false
-    console.error('❌Exited with code:', code)
-    start('main.js'); //
+    console.error(chalk.red(`❌Exited with code: ${code}`))
 
     if (code === 0) return
-    watchFile(args[0], () => {
-      unwatchFile(args[0])
-      start(file)
+
+    fs.watchFile(args[0], () => {
+      fs.unwatchFile(args[0])
+      start('main.js')
     })
   })
 
-  //---
-  console.log(chalk.yellow(`🖥️ ${os.type()}, ${os.release()} - ${os.arch()}`));
-  const ramInGB = os.totalmem() / (1024 * 1024 * 1024);
-  console.log(chalk.yellow(`💾 Total RAM: ${ramInGB.toFixed(2)} GB`));
-  const freeRamInGB = os.freemem() / (1024 * 1024 * 1024);
-  console.log(chalk.yellow(`💽 Free RAM: ${freeRamInGB.toFixed(2)} GB`));
-  console.log(chalk.yellow(`📃 OWNER PRINCE DASTAGEER`));
+  p.on('error', err => {
+    console.error(chalk.red(`Error: ${err}`))
+    p.kill()
+    isRunning = false
+    start('main.js')
+  })
 
-  const packageJsonPath = path.join(path.dirname(currentFilePath), './package.json');
+  const pluginsFolder = path.join(path.dirname(currentFilePath), 'plugins')
+
+  fs.readdir(pluginsFolder, async (err, files) => {
+    if (err) {
+      console.error(chalk.red(`Error reading plugins folder: ${err}`))
+      return
+    }
+    console.log(chalk.yellow(`Installed ${files.length} plugins`))
+
     try {
-    const packageJsonData = await fsPromises.readFile(packageJsonPath, 'utf-8');
-    const packageJsonObj = JSON.parse(packageJsonData);
-    console.log(chalk.blue.bold(`\n📦 Package Information`));
-    console.log(chalk.cyan(`Number: ${packageJsonObj.name}`));
-    console.log(chalk.cyan(`Version: ${packageJsonObj.version}`));
-    console.log(chalk.cyan(`Description: ${packageJsonObj.description}`));
-    console.log(chalk.cyan(`Author: ${packageJsonObj.author.name}`));
-  } catch (err) {
-    console.error(chalk.red(`❌Could not read the file package.json: ${err}`));
-  }
-
-
-  console.log(chalk.blue.bold(`\n⏰ Current time`));
-  const currentTime = new Date().toLocaleString('en-US', { timeZone: 'Asia/Karachi' })
-  //const currentTime = new Date().toLocaleString();
-  console.log(chalk.cyan(`${currentTime}`));
-
-  setInterval(() => {}, 1000);
-
-  
-
-  //----
-  let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-  if (!opts['test'])
-    if (!rl.listenerCount()) rl.on('line', line => {
-      p.emit('message', line.trim())
-    })
-  // console.log(p)
+      const { default: baileys } = await import('@whiskeysockets/baileys')
+      const version = (await baileys.fetchLatestBaileysVersion()).version
+      console.log(chalk.yellow(`Using Baileys version ${version}`))
+    } catch (e) {
+      console.error(chalk.red(' Baileys library is not installed'))
+    }
+  })
 }
 
 start('main.js')
+
+process.on('unhandledRejection', () => {
+  console.error(chalk.red(`Unhandled promise rejection. Bot will restart...`))
+  start('main.js')
+})
+
+process.on('exit', code => {
+  console.error(chalk.red(`Exited with code: ${code}`))
+  console.error(chalk.red(`Bot will restart...`))
+  start('main.js')
+})
