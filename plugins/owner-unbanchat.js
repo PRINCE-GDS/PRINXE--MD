3c7592297d187aa5afe8561ad693473e4cb6c7f1
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-09-15 16:50:52 +0500

    𝑷-𝑴𝑫

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,12 +1,17 @@
 //import db from '../lib/database.js'
 
-let handler = async (m, { conn, isOwner, isAdmin, isROwner} ) => {
+let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    m.reply('✅Tʜᴇ ʙᴏᴛ ʜᴀs ʙᴇᴇɴ ᴀᴄᴛɪᴠᴀᴛᴇᴅ ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ𒁂')   
+    m.reply('✅Tʜᴇ ʙᴏᴛ ʜᴀs ʙᴇᴇɴ ᴀᴄᴛɪᴠᴀᴛᴇᴅ ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ𒁂')
+
+    // Save the updated database
+    if (global.db && typeof global.db.save === 'function') {
+        await global.db.save()
+    }
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
-handler.command = ['boton', 'unbanchat'] 
+handler.command = ['boton', 'unbanchat']
 
 export default handler
