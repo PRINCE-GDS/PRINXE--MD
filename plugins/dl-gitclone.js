5707110a43e7372413bb018c3cd9befce683ea08
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-09-28 13:02:09 +0500

    𝑷-𝑴𝑫

--- plugins/dl-gitclone.js
+++ plugins/dl-gitclone.js
@@ -2,7 +2,7 @@
 import fetch from 'node-fetch'
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `where is the github link?\n\n📌 Example : ${usedPrefix + command} https://github.com/Guru322/api`
+    if (!args[0]) throw `where is the github link?\n\n📌 Example : ${usedPrefix + command} https://github.com/PRINCE-GDS/PRINXE-MD`
     if (!regex.test(args[0])) throw '⚠️ link incorrect'
     let [_, user, repo] = args[0].match(regex) || []
     repo = repo.replace(/.git$/, '')
@@ -15,6 +15,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 handler.help = ['gitclone <url>']
 handler.tags = ['downloader']
 handler.command = ['gitclone'] 
-handler.credit = true
+handler.credit = false
 
 export default handler
