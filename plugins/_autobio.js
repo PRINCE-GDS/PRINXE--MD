6ea0dfebeb499911da05c4248cbcf3469e086d2c
STRIKERBOY <110353280+Devenlee350@users.noreply.github.com>
2024-10-09 18:52:30 +0200

    𝚂𝚃𝚁𝙸𝙺𝙴𝚁𝙱𝙾𝚈

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -14,7 +14,7 @@ handler.all = async function (m) {
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n⌚ Time Active ${muptime}\n\n ┃ 🛡️ᑭᖇIᑎᑕᕮ ᗷOT ᗰᗪ🛡️`
+		let bio = `\n⌚ Time Active ${muptime}\n\n ┃ 🛡️ɴᴏᴠᴀ ᵛ4🛡️`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
   }
